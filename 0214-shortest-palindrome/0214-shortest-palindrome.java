39c957ea87eeb0002f555b97afc4cf1905f0406f
Ankit Vishwakarma | Aspiring Software Developer <106132327+ankitvishwakarma91@users.noreply.github.com>
2026-07-28 15:44:00 +0530

    Time: 495 ms (5.04%), Space: 45.9 MB (99.25%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+                int n= s.length();
+        String rev = new StringBuilder(s).reverse().toString();
+        
+        for (int i = 0; i < n; i++) {
+            if (
+                s.substring(0,n-i).equals(rev.substring(i))
+            ) {
+                return new StringBuilder(rev.substring(0,i))
+                .append(s)
+                .toString();
+            }
+        }
+        return "";
+    }
+}
