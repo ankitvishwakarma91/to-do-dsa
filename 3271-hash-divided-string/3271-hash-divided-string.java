e3e216e1064d9b290406402e47fa1a0b79ea8d74
Ankit Vishwakarma | Aspiring Software Developer <106132327+ankitvishwakarma91@users.noreply.github.com>
2026-09-25 17:37:05 +0530

    Time: 2 ms (96.82%), Space: 46.2 MB (86.4%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.java
+++ 3271-hash-divided-string/3271-hash-divided-string.java
@@ -2,23 +2,37 @@ class Solution {
     public String stringHash(String s, int k) {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i <= s.length() - k; i += k) {
+        // Brute force
+        // for (int i = 0; i <= s.length() - k; i += k) {
 
-            String curr = s.substring(i, i + k);
-            int sum = 0;
-            for (char ch : curr.toCharArray()) {
-                int val = ch - 'a';
-                sum += val;
+        //     String curr = s.substring(i, i + k);
+        //     int sum = 0;
+        //     for (char ch : curr.toCharArray()) {
+        //         int val = ch - 'a';
+        //         sum += val;
+        //     }
+        //     System.out.println("sum " + sum);
+        //     int rem = sum % 26;
+
+        //     System.out.print(rem);
+        //     System.out.println((char) ('a' + rem));
+
+        //     sb.append((char) ('a' + rem));
+        // }
+
+        int sum = 0;
+        int count = 0;
+        for(int i = 0; i < s.length() ; i++){
+            sum += s.charAt(i) - 'a';
+            count++;
+            if(count == k){
+                int rem = sum % 26;
+                char ch = (char) ('a' + rem);
+                sb.append(ch);
+                sum = 0;
+                count = 0;
             }
-            System.out.println("sum " + sum);
-            int rem = sum % 26;
-
-            System.out.print(rem);
-            System.out.println((char) ('a' + rem));
-
-            sb.append((char) ('a' + rem));
         }
-
         return sb.toString();
     }
 }
